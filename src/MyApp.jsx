00f2d7f4ebfec6137a8afc632bb955f652793087
Chanchai Thaiyanon <chanchai.th@nisdt.co.th>
2024-10-27 16:11:29 +0700

    shared parent

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -4,6 +4,19 @@ function MyButton() {
   return <button>I'm a button</button>;
 }
 
+function AboutPage() {
+  return (
+    <>
+      <h1>about</h1>
+      <p>
+        Hello there.
+        <br />
+        How do you do?
+      </p>
+    </>
+  );
+}
+
 const MyApp = () => {
   return (
     <div>

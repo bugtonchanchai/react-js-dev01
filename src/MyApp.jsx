46f2b67e29e658968ec4ac385ddbc97337cee17b
Chanchai Thaiyanon <chanchai.th@nisdt.co.th>
2024-10-27 17:00:12 +0700

    Rendering lists

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import img from "./img.jpg";
 
-let isLoggedIn = true;
-
 const user = {
   name: "Mark Zuckerberg",
   imageUrl:
     "https://upload.wikimedia.org/wikipedia/commons/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg",
   imageSize: 200,
 };
+
+const isLoggedIn = true;
+
+const products = [
+  { title: "Cabbage", id: 1 },
+  { title: "Garlic", id: 2 },
+  { title: "Apple", id: 3 },
+];
+
+const listItems = products.map((product) => (
+  <li key={product.id}>{product.title}</li>
+));
 
 function MyButton() {
   return <button>I'm a button</button>;
@@ -46,6 +56,8 @@ const MyApp = () => {
       <hr />
       {isLoggedIn ? <h1>Logged In</h1> : <h1>Login form</h1>}
       {isLoggedIn && <h1>Logged In</h1>}
+      <hr />
+      <ul>{listItems}</ul>
     </>
   );
 };

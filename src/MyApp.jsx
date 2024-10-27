c9274e791da754149fd36f1f1602146f8d38c058
Chanchai Thaiyanon <chanchai.th@nisdt.co.th>
2024-10-27 17:01:51 +0700

    Rendering lists (style)

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -11,13 +11,20 @@ const user = {
 const isLoggedIn = true;
 
 const products = [
-  { title: "Cabbage", id: 1 },
-  { title: "Garlic", id: 2 },
-  { title: "Apple", id: 3 },
+  { title: "Cabbage", isFruit: false, id: 1 },
+  { title: "Garlic", isFruit: false, id: 2 },
+  { title: "Apple", isFruit: true, id: 3 },
 ];
 
 const listItems = products.map((product) => (
-  <li key={product.id}>{product.title}</li>
+  <li
+    key={product.id}
+    style={{
+      color: product.isFruit ? "magenta" : "darkred",
+    }}
+  >
+    {product.title}
+  </li>
 ));
 
 function MyButton() {
